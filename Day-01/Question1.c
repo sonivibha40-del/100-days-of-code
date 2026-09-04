7c3732a775f4dbf387c310d21ff47f13067d0dc7
Vibha Soni <sonivibha40@gmail.com>
2026-09-04 17:38:03 +0000

    Day 01 - Add Question1 and Question2

---
+++ Day-01/Question1.c
@@ -0,0 +1,11 @@
+//Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main(){
+   int a,b;
+   printf("Enter your first no.: ");
+   scanf("%d",&a);
+   printf("Enter your second no.: ");
+   scanf("%d",&b);
+   printf("Sum: %d\n",a+b);
+   return 0;
+}
---
+++ Day-01/Question2.c
@@ -0,0 +1,14 @@
+//Write a program to input two numbers and display their sum, difference, product>
+#include<stdio.h>
+int main(){
+   int a,b;
+   printf("Enter your first no.: ");
+   scanf("%d",&a);
+   printf("Enter your second no.: ");
+   scanf("%d",&b);
+   printf("sum: %d\n",a+b);
+   printf("diff: %d\n",a-b);
+   printf("product: %d\n",a*b);
+   printf("divide: %d\n",a/b);
+   return 0;
+}
