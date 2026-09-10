55991ee55087d762e1ef7a2f2fbe78d7999581fb
Vibha Soni <sonivibha40@gmail.com>
2026-09-10 08:30:07 +0000

    Day 14 - Add Question 1 and Question 2

---
+++ Day-14/Question1.c
@@ -0,0 +1,12 @@
+//Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= 2*n-1; i += 2) {
+        sum += i;
+    }
+    printf("Sum = %d\n", sum);
+    return 0;
+}
