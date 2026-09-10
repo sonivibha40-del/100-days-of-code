3c1fa9ee54552e851c18d2a1042af8428b0c568e
Vibha Soni <sonivibha40@gmail.com>
2026-09-10 16:43:57 +0000

    Day 15 - Add Question 1 and Question 2

---
+++ Day-15/Question1.c
@@ -0,0 +1,12 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main() {
+    int n, fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial = %d\n", fact);
+    return 0;
+}
---
+++ Day-15/Question2.c
@@ -0,0 +1,15 @@
+//Write a program to reverse a given number.
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    printf("Reverse = %d\n", rev);
+    return 0;
+}
